230cdcb10c0645d4cb10ed38271b4a693e1c852d
Vinicius Silva Alencar <vinicius.s.alencar@outlook.com>
2024-03-06 08:36:53 -0400

    adding CoreService structure

--- tennis-league/Application/AppDelegate.swift
+++ tennis-league/Application/AppDelegate.swift
@@ -1,16 +1,12 @@
-//
-//  AppDelegate.swift
-//  tennis-league
-//
-//  Created by Vinicius Alencar on 03/03/24.
-//
-
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
